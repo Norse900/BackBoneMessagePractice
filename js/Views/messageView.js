26c4bd709d89b80decaf965bcac39eac43ad42dc
montgomery <james.montgomery@fusionalliance.com>
2014-02-18 10:57:15 -0500

    template now rendering the object's view

--- js/Views/messageView.js
+++ js/Views/messageView.js
@@ -5,24 +5,23 @@ $(function () {
     var secretMessage = App.SecretMessage;
     var mainMessageView = Backbone.View.extend({
         tagName: 'li',
-        el: '#messagesList',
+        el: '#dateSentMessageBodyInfo',
         model: secretMessage,
-        template: _.template($("#messagesContentDisplay").html(),{model : secretMessage}),
+        //template: _.template($("#messagesContentDisplay").html(),{model : secretMessage}),
         template2: _.template("dateSent : <%= model['dateSent'] %>  Message Body : <%= model['body'] %>"),
         initialize: function () {
             this.render(secretMessage);
             return this;
         },
         render: function (myModel) {
-            var item = myModel;
-            console.log("template2 : " + this.template2({model : myModel.attributes}));
-            var placeholder = '';
-            this.$el.html(this.template(myModel.toJSON()));
 
-            return this;
+            //console.log("template2 : " + this.template2({model : myModel.attributes}));
+            var template = _.template($("#messagesContentDisplay").html(), myModel.toJSON());
+            this.$el.html(template);
+return this;
         }
     });
 
-    App.MainMessageView = new mainMessageView({model: secretMessage, el: $("#messagesList")});
+    App.MainMessageView = new mainMessageView({model: secretMessage, el: $("#dateSentMessageBodyInfo")});
 
 });
