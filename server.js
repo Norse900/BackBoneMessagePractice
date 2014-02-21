c634656d386943e8da5150386cf30d470e62f28e
montgomery <james.montgomery@fusionalliance.com>
2014-02-21 11:28:39 -0500

    Draft Message model and schema added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var application_root = __dirname,
 //Create server
 var App = express();
 
-
+//getting cluttered. Want to move the model data out.
 var message = new mongoose.Schema({
     title: String,
     body: String,
@@ -23,14 +23,29 @@ var contact = new mongoose.Schema({
     name:String,
     contactAddress: String
 });
+
 var ContactModel = mongoose.model('contact', contact);
 var currentMessage = new mongoose.Schema({
     id:String
 });
+
 var CurrentMessageModel = new mongoose.model('currentMesage', currentMessage);
+
+var draftMessage = new mongoose.Schema({
+    title: String,
+    body: String,
+    owner: String,
+    recipient: String,
+    status: String,
+    dateSent: String
+});
+var DraftMessageModel = mongoose.model('draftMessage', draftMessage);
+
 App.get('/api', function (req, res) {
     res.send('Welcome');
 });
+
+
 //eventually, I will need to implement something similar to the Command Pattern
 //to get all of the needed model data from the different dbs.
 App.get('/messages', function(req,res){
